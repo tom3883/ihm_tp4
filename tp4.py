286ad3507cd3a37fb8b9d810be1544fe53127fcc
Humblot Stephane <stephane.humblot@iut2.univ-grenoble-alpes.fr>
2021-04-11 00:22:54 +0200

    ecriture en append

--- tp4.py
+++ tp4.py
@@ -102,7 +102,7 @@ def setDiscs():
 
 def testEnd():
 
-    with open('results.csv', mode='w', newline='') as results_file:
+    with open('results.csv', mode='a', newline='') as results_file:
 
         fields = ["nom", "technique", "ID", "temps", "erreur"]
         results_writer = csv.DictWriter(results_file, fieldnames=fields)
